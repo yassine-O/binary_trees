7655ca229e7779ad37bedc5f0554ff47850eea01
Yassine <yassine@gmail.com>
2024-02-29 02:47:26 -0800

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,10 +11,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
 
-	return node;
+	return (node);
 }
