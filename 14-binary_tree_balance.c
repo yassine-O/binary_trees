a917553f4eaed828496ba9333b5fc4bb3064771e
Yassine <yassine@gmail.com>
2024-02-29 15:47:16 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,15 +12,28 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-	{
-		hl += 1 + binary_tree_balance(tree->left);
-	}
-
-	if (tree->right != NULL)
-	{
-		hr += 1 + binary_tree_balance(tree->right);
-	}
+	hl = binary_tree_height(tree->left);
+	hr = binary_tree_height(tree->right);
 
 	return (hl - hr);
+}
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+/**
+ * binary_tree_height - Description
+ * @tree: Pointer
+ * Return: void
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int hl = 0, hr = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	hl += 1 + binary_tree_height(tree->left);
+	hr += 1 + binary_tree_height(tree->right);
+
+	return (MAX(hl, hr));
 }
